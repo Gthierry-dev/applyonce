e74829f34107455c6ad45b12e4716d8c96fe904f
gusenga thierry <127823871+draken250@users.noreply.github.com>
2025-05-22 13:46:07 +0200

    rough one

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import Layout from '@/components/layout/Layout';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { FileText, BookOpen, HelpCircle, Newspaper } from 'lucide-react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
